73538d5549454b2ef382b6e8ab7509aefc3fdeea
Sarah Donato <47866580+CrimsonSarah@users.noreply.github.com>
2022-12-08 17:09:41 -0300

    Add files via upload

--- Trabalho7/JS/script.js
+++ Trabalho7/JS/script.js
@@ -28,23 +28,35 @@ async function searchArtist () {
         element.remove();
     });
 
-    try {
-        let result = await fetch(url +"search/multi?q="+artistsearch.value, options);
-        let js = await result.json();
-        let apipath = js.response.sections[3].hits[0].result.api_path
-        let result2 = await fetch(url+apipath, options);
-        let js2 = await result2.json();
+    if (artistsearch.value != "") {
+        try {
+            let result = await fetch(url + "search/multi?q=" + artistsearch.value, options);
+            let js = await result.json();
+            let apipath = js.response.sections[3].hits[0].result.api_path
+            let result2 = await fetch(url + apipath, options);
+            let js2 = await result2.json();
 
-        let artistname = document.createElement("p");
-        artistname.className = "artistsearchresult";
-        artistsection.appendChild(artistname);
-        artistname.innerHTML = js2.response.artist.name;
+            let artistname = document.createElement("p");
+            artistname.className = "artistsearchresult";
+            artistsection.appendChild(artistname);
+            artistname.innerHTML = js2.response.artist.name;
 
-        let artistdescription = document.createElement("p");
-        artistdescription.className = "artistsearchresult";
-        artistsection.appendChild(artistdescription);
-        artistdescription.innerHTML = js2.response.artist.description_preview;
-    } catch {
+            let artistdescription = document.createElement("p");
+            artistdescription.className = "artistsearchresult";
+            artistsection.appendChild(artistdescription);
+            artistdescription.innerHTML = js2.response.artist.description_preview;
+        } catch {
+            let results = document.querySelectorAll(".artistsearchresult");
+            results.forEach(element => {
+                element.remove();
+            });
+
+            let errormsg = document.createElement("p");
+            errormsg.className = "artistsearchresult";
+            artistsection.appendChild(errormsg);
+            errormsg.innerHTML = "No artists found. Please try again in a few moments.";
+        }
+    } else {
         let results = document.querySelectorAll(".artistsearchresult");
         results.forEach(element => {
             element.remove();
@@ -53,7 +65,7 @@ async function searchArtist () {
         let errormsg = document.createElement("p");
         errormsg.className = "artistsearchresult";
         artistsection.appendChild(errormsg);
-        errormsg.innerHTML = "No artists found. Please try again in a few moments.";
+        errormsg.innerHTML = "Please type something to search for.";
     }
 }
 
@@ -63,29 +75,41 @@ async function searchAlbum () {
         element.remove();
     });
 
-    try {
-        let result = await fetch(url + "search/multi?q=" + albumsearch.value, options);
-        let js = await result.json();
-        let apipath = js.response.sections[4].hits[0].result.api_path
-        let result2 = await fetch(url + apipath, options);
-        let js2 = await result2.json();
-        console.log(js2);
+    if (albumsearch.value != "") {
+        try {
+            let result = await fetch(url + "search/multi?q=" + albumsearch.value, options);
+            let js = await result.json();
+            let apipath = js.response.sections[4].hits[0].result.api_path
+            let result2 = await fetch(url + apipath, options);
+            let js2 = await result2.json();
+            console.log(js2);
 
-        let albumname = document.createElement("p");
-        albumname.className = "albumsearchresult";
-        albumsection.appendChild(albumname);
-        albumname.innerHTML = js2.response.album.full_title
+            let albumname = document.createElement("p");
+            albumname.className = "albumsearchresult";
+            albumsection.appendChild(albumname);
+            albumname.innerHTML = js2.response.album.full_title
 
-        let albumrelease = document.createElement("p");
-        albumrelease.className = "albumsearchresult";
-        albumsection.appendChild(albumrelease);
-        albumrelease.innerHTML = js2.response.album.release_date_components.day + "/" + js2.response.album.release_date_components.month + "/" + js2.response.album.release_date_components.year;
+            let albumrelease = document.createElement("p");
+            albumrelease.className = "albumsearchresult";
+            albumsection.appendChild(albumrelease);
+            albumrelease.innerHTML = js2.response.album.release_date_components.day + "/" + js2.response.album.release_date_components.month + "/" + js2.response.album.release_date_components.year;
 
-        let albumdescription = document.createElement("p");
-        albumdescription.className = "albumsearchresult";
-        albumsection.appendChild(albumdescription);
-        albumdescription.innerHTML = js2.response.album.description_preview
-    } catch {
+            let albumdescription = document.createElement("p");
+            albumdescription.className = "albumsearchresult";
+            albumsection.appendChild(albumdescription);
+            albumdescription.innerHTML = js2.response.album.description_preview
+        } catch {
+            let results = document.querySelectorAll(".albumsearchresult");
+            results.forEach(element => {
+                element.remove();
+            });
+
+            let errormsg = document.createElement("p");
+            errormsg.className = "albumsearchresult";
+            albumsection.appendChild(errormsg);
+            errormsg.innerHTML = "No albuns found. Please try again in a few moments.";
+        }
+    } else {
         let results = document.querySelectorAll(".albumsearchresult");
         results.forEach(element => {
             element.remove();
@@ -94,7 +118,7 @@ async function searchAlbum () {
         let errormsg = document.createElement("p");
         errormsg.className = "albumsearchresult";
         albumsection.appendChild(errormsg);
-        errormsg.innerHTML = "No albuns found. Please try again in a few moments.";
+        errormsg.innerHTML = "Please type something to search for.";
     }
 }
 
@@ -104,22 +128,34 @@ async function searchSong () {
         element.remove();
     });
 
-    try {
-        let result = await fetch(url +"search/multi?q="+songsearch.value, options);
-        let js = await result.json();
-        let result2 = await fetch(url + "songs/" + js.response.sections[1].hits[0].result.id + "/lyrics", options);
-        let js2 = await result2.json();
+    if(songsearch.value != "") {
+        try {
+            let result = await fetch(url + "search/multi?q=" + songsearch.value, options);
+            let js = await result.json();
+            let result2 = await fetch(url + "songs/" + js.response.sections[1].hits[0].result.id + "/lyrics", options);
+            let js2 = await result2.json();
 
-        let songname = document.createElement("p");
-        songname.className = "songsearchresult";
-        songsection.appendChild(songname);
-        songname.innerHTML = js.response.sections[1].hits[0].result.full_title;
+            let songname = document.createElement("p");
+            songname.className = "songsearchresult";
+            songsection.appendChild(songname);
+            songname.innerHTML = js.response.sections[1].hits[0].result.full_title;
 
-        let lyrics = document.createElement("p");
-        lyrics.className = "songsearchresult";
-        songsection.appendChild(lyrics);
-        lyrics.innerHTML = js2.response.lyrics.lyrics.body.html;
-    } catch {
+            let lyrics = document.createElement("p");
+            lyrics.className = "songsearchresult";
+            songsection.appendChild(lyrics);
+            lyrics.innerHTML = js2.response.lyrics.lyrics.body.html;
+        } catch {
+            let results = document.querySelectorAll(".songsearchresult");
+            results.forEach(element => {
+                element.remove();
+            });
+
+            let errormsg = document.createElement("p");
+            errormsg.className = "songsearchresult";
+            songsection.appendChild(errormsg);
+            errormsg.innerHTML = "No songs or lyrics found. Please try again in a few moments.";
+        }
+    } else {
         let results = document.querySelectorAll(".songsearchresult");
         results.forEach(element => {
             element.remove();
@@ -128,7 +164,7 @@ async function searchSong () {
         let errormsg = document.createElement("p");
         errormsg.className = "songsearchresult";
         songsection.appendChild(errormsg);
-        errormsg.innerHTML = "No songs or lyrics found. Please try again in a few moments.";
+        errormsg.innerHTML = "Please type something to search for.";
     }
 }
 
